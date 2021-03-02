b64542ace3b7aa0768377593f82afc458ed29e86
DeboraSamara <deborah50play@gmail.com>
2021-03-01 22:31:03 -0300

    Primeira versão

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require ('express');
+
+const app = express ();
+
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+const users = require('./modelos/users');
+
+const url = 'mongodb+srv://usuario2021:senha489@cluster0.tb4lv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
+const options = {
+    reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true
+}
+
+mongoose.connect(url, options);
+
+mongoose.connection.on('connected', () =>
+    {
+
+    console.log("Aplicação conectada ao banco de dados!");
+    
+    })
+    
+    mongoose.connection.on('error', (err) => {
+    
+    console.log("Erro na conexão com banco de dados:"+err);
+    
+    })
+    
+    mongoose.connection.on('disconnected', () => {
+    
+    console.log("Aplicação desconectada do banco de dados!");
+    
+    })
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+/**app.get('/', (req, res) => {
+
+    let obj = req.query;
+    
+    res.send(`Hello World! Você enviou nome ${obj.nome} e idade ${obj.idade}`)
+    
+})**/
+
+app.listen(3000, () => {
+
+    console.log(`Example app listening at http://localhost:3000`)
+    
+})
+
+app.get('/', (req, res) => {
+
+    users.find({}, (err, data) => {
+    
+        if (err) return res.send({ error: 'Erro na consulta de usuário!'});
+    
+        return res.send(data);
+    
+    });
+    
+})
+
+app.post('/create', (req, res) => {
+        const {nome, email, senha} = req.body; 
+        if (!nome || !email || !senha) return res.send({error: 'Dados insuficientes!'});
+        users.findOne({email}, (err, data) => {
+            if (err) return res.send({error: 'Erro ao buscar usuáio!'});
+            if(data) return res.send({error: 'usuário já cadastrado'});
+            users.create(req.body, (err, data) =>{
+                if (err) return res.send({error: 'Erro ao criar usuário'});
+                data.senha = undefined;
+                return res.send(data);
+            });
+        });
+    })
+    
+
---
+++ modelos/users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+    nome: {type: String, required:true},
+
+    email: {type: String, required:true, unique: true, lowercase: true},
+
+    senha:{type: String, required:true}
+
+});
+
+module.exports = mongoose.model('User', UserSchema);
