735a31fb21b199f66d7921184df9db3cabc51e16
DeboraSamara <deborah50play@gmail.com>
2021-03-06 14:52:17 -0300

    Terceiro envio

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require ('express');
+const express = require('express');
 
-const app = express ();
+const app = express();
 
 const mongoose = require('mongoose');
 
@@ -8,7 +8,13 @@ const bodyParser = require('body-parser');
 
 const users = require('./modelos/users');
 
+const jwt = require('jsonwebtoken');
+
 const url = 'mongodb+srv://usuario2021:senha489@cluster0.tb4lv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const bcrypt = require('bcrypt');
+
+const auth = require('./auth');
 
 
 const options = {
@@ -17,27 +23,26 @@ const options = {
 
 mongoose.connect(url, options);
 
-mongoose.connection.on('connected', () =>
-    {
+mongoose.connection.on('connected', () => {
 
     console.log("Aplicação conectada ao banco de dados!");
-    
-    })
-    
-    mongoose.connection.on('error', (err) => {
-    
-    console.log("Erro na conexão com banco de dados:"+err);
-    
-    })
-    
-    mongoose.connection.on('disconnected', () => {
-    
+
+})
+
+mongoose.connection.on('error', (err) => {
+
+    console.log("Erro na conexão com banco de dados:" + err);
+
+})
+
+mongoose.connection.on('disconnected', () => {
+
     console.log("Aplicação desconectada do banco de dados!");
-    
-    })
+
+})
 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
@@ -52,33 +57,64 @@ app.use(bodyParser.json());
 app.listen(3000, () => {
 
     console.log(`Example app listening at http://localhost:3000`)
-    
+
 })
 
-app.get('/', (req, res) => {
+app.get('/', auth, (req, res) => {
 
     users.find({}, (err, data) => {
-    
-        if (err) return res.send({ error: 'Erro na consulta de usuário!'});
-    
+
+        if (err) return res.send({ error: 'Erro na consulta de usuário!' });
+
         return res.send(data);
-    
+
     });
-    
+
 })
 
-app.post('/create', (req, res) => {
-        const {nome, email, senha} = req.body; 
-        if (!nome || !email || !senha) return res.send({error: 'Dados insuficientes!'});
-        users.findOne({email}, (err, data) => {
-            if (err) return res.send({error: 'Erro ao buscar usuáio!'});
-            if(data) return res.send({error: 'usuário já cadastrado'});
-            users.create(req.body, (err, data) =>{
-                if (err) return res.send({error: 'Erro ao criar usuário'});
-                data.senha = undefined;
-                return res.send(data);
-            });
-        });
-    })
-    
+app.post('/create', auth, (req, res) => {
+
+    const { nome, email, senha } = req.body;
+    if (!nome || !email || !senha) return res.send({ error: 'Dados insuficientes!' });
+    users.findOne({ email }, (err, data) => {
+        if (err) return res.send({ error: 'Erro ao buscar usuáio!' });
+        if (data) return res.send({ error: 'usuário já cadastrado' });
+        users.create(req.body, (err, data) => {
+            if (err) return res.send({ error: 'Erro ao criar usuário' });
+            data.senha = undefined;
+            return res.send({ data, token: createUserToken(data.id) });
+
+        });
+    });
+})
+
+app.post('/auth', auth, (req, res) => {
+
+    const { email, senha } = req.body;
+
+    if (!email || !senha) return res.send({ error: 'Dados insuficientes!' });
+
+    users.findOne({ email }, (err, data) => {
+
+        if (err) return res.send({ error: 'Erro ao buscar usuário!' });
+
+        if (!data) return res.send({ error: 'Usuário não registrado' });
+
+        bcrypt.compare(senha, data.senha, (err, same) => {
+
+            if (!same) return res.send({ error: 'Erro ao autenticar usuário!' });
+
+            return res.send({ data, token: createUserToken(data.id) });
+
+        });
+
+    });
+
+});
+
+const createUserToken = (userId) => {
+
+    return jwt.sign({ id: userId }, 'umasenha', { expiresIn: '7d' });
+
+}
 
--- modelos/users.js
+++ modelos/users.js
@@ -2,13 +2,30 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const bcrypt = require('bcrypt');
+
+
 const UserSchema = new Schema({
 
-    nome: {type: String, required:true},
+    nome: { type: String, required: true },
 
-    email: {type: String, required:true, unique: true, lowercase: true},
+    email: { type: String, required: true, unique: true, lowercase: true },
 
-    senha:{type: String, required:true}
+    senha: { type: String, required: true }
+
+});
+
+UserSchema.pre('save', function (next) {
+
+    let user = this;
+
+    bcrypt.hash(user.senha, 10, (err, encrypted) => {
+
+        user.senha = encrypted;
+
+        return next();
+
+    });
 
 });
 
